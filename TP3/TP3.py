c299472a517d50fed2059a0a29cc21bf9e54b1f3
Thomas-projet <thomas.callegari@efrei.net>
2021-12-07 16:25:39 +0100

    test_taskIdExist

--- TP3/TP3.py
+++ TP3/TP3.py
@@ -2,10 +2,11 @@ import unittest
 
 
 class Task:
+    
     def __init__(self, description):
         self.description = description
         self.status = True
-
+        
 
 
 class Test(unittest.TestCase):
@@ -16,6 +17,10 @@ class Test(unittest.TestCase):
     def test_taskStatusExist(self):
         task = Task("learn python")
         self.assertEqual(True, task.status)
+
+    def test_taskIdExist(self):
+        task = Task("learn python", 0)
+        self.assertEqual(0, task.id)
 
 
 if __name__ == '__main__':
